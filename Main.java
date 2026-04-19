ac2bb4cfbfe089c1a7c66eae351e8852f8ca7dcc
Precious Phele <bontlephele22@gmail.com>
2026-04-19 13:46:20 -0700

    'POE Part 1-Login validation

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     
+   
 
     public static boolean checkUsername(String username) {
         return username.contains("_") && username.length() <= 5;
@@ -10,16 +11,23 @@ public class Main {
 
          public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-    
-
-   
-
-        System.out.println("=== Create Account ===");
-
-        // Username
+        
+        //Registration
+        
+        System.out.print("Enter your first name: ");
+        String firstName = input.nextLine();
+        
+        System.out.print("Enter your last name: ");
+        String lastName = input.nextLine();
+        
         System.out.print("Enter username: ");
         String username = input.nextLine();
+        
+        //Username
 
+        
+
+        
         // Check username conditions
         if (username.contains("_") && username.length() <= 5) {
             System.out.println("Username successfully captured");
@@ -28,10 +36,15 @@ public class Main {
         }
 
 
-       
-        // Password
+        
+        
+        
         System.out.print("Enter password: ");
         String password = input.nextLine();
+        
+        
+        // Password
+       
 
         // flags
         boolean isLongEnough = false;
@@ -58,22 +71,61 @@ public class Main {
         }
 
         if (isLongEnough && hasNumber && hasUpperCase && hasSpecialCase) {
-            System.out.println("Password successfully captured");
+            System.out.println("Password successfully added");
         } else {
             System.out.println("Password is not correctly formatted, please ensure that the password contains at least eight characters, a capital letter, a number and a special character.");
         }
+        
+        
+        
+        System.out.print("Enter cell phone number: ");
+        String cellNumber = input.nextLine();
+        
+         // Must start with + and have ≤10 digits after the country code
 
+        if (cellNumber.startsWith("+") && cellNumber.substring(3).length() <= 10) {
+            System.out.println("Cell phone number successfully added.");
+        } else {
+            System.out.println("Cell phone number incorrectly formatted or does not contain international code.");
+        }
+    
+        
+        
+        
+        
+        
+        
+        //login user
+        
+         Login userLogin = new Login(); 
+        System.out.print("Enter username to login: ");
+        String loginUser = input.nextLine();
+        System.out.print("Enter password to login: ");
+        String loginPass = input.nextLine();
+
+        if (userLogin.checkLogin(loginUser, loginPass, username, password)) {
+            System.out.println("Welcome " + firstName + ", " + lastName + " it is great to see you again.");
+        } else {
+            System.out.println("Username or password incorrect, please try again.");
+            
+}
+
+
+    
+    
+       
+
+       
        
        
         //Phone number 
         
-        System.out.print("Enter South African cell phone number: ");
-        String cellNumber = input.nextLine();
+       
 
         // Checking cell number conditions
         boolean cellNumberValid = false;
 
-        if(cellNumber.length() == 12 && cellNumber.startsWith("+27")) { // FIX 2: add ()
+        if(cellNumber.length() == 12 && cellNumber.startsWith("+27")) {
             cellNumberValid = true;
 
             for (int i = 3; i < 12; i++) {
@@ -89,9 +141,16 @@ public class Main {
         } else {
             System.out.println("Cell number is not correctly formatted; please ensure that the cell number starts with +27 followed by 9 digits.");
         }
-    
+
+        
 
 
         input.close();
+    }
+}
+
+class Login {
+    public boolean checkLogin(String loginUser, String loginPass, String username, String password) {
+        return loginUser.equals(username) && loginPass.equals(password);
     }
 }
